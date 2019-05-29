8557c6b654c174a3203ec929452f0bcc168845aa
amG <bomiishere@gmail.com>
2019-05-29 11:08:47 +0800

    Initial Commit

---
+++ SmileSelfie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmileSelfie
+//
+//  Created by Wu on 2019/5/29.
+//  Copyright © 2019 amG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
