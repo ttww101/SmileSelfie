a2e68416935ff26682cba9615bd963920ac6cf7a
amG <bomiishere@gmail.com>
2019-05-29 11:50:51 +0800

    add vc controller

---
+++ SmileSelfie/Controllers/HomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewController.swift
+//  SmileSelfie
+//
+//  Created by Wu on 2019/5/29.
+//  Copyright © 2019 amG. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = UIColor.clear
+    }
+    
+    @IBAction func smileButtonDidTouchupInside() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "SmileSelfieViewController")
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+
+
+}
+
---
+++ SmileSelfie/Controllers/SmileSelfieViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SmileSelfieViewController.swift
+//  SmileSelfie
+//
+//  Created by Wu on 2019/5/29.
+//  Copyright © 2019 amG. All rights reserved.
+//
+
+import UIKit
+
+class SmileSelfieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = UIColor.clear
+    }
+
+}
